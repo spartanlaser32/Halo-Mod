b4e6de35fbed1fba2c2cdb99b97759a547ab08ef
spartanlaser32 <ericj1702@gmail.com>
2026-04-09 12:01:08 -0500

    added some Halo 4 weapons and some others

---
+++ guns/Halo Reach/halo-reach-rocket-launcher.lua
@@ -0,0 +1,85 @@
+local item_sounds = require("__base__.prototypes.item_sounds")
+data:extend{
+    {
+        type = "ammo-category",
+        name = "halo-reach-rocket-launcher-ammo",
+        subgroup = "ammo-category"
+    },{
+        type = "ammo",
+        name = "halo-reach-rocket-launcher-ammo",
+        icons = {{ icon = "__base__/graphics/icons/rocket.png"},{ icon = "__Halo-Mod__/graphics/icons/halo-reach-rocket-launcher.png"}},
+        ammo_category = "halo-reach-rocket-launcher-ammo",
+        ammo_type =
+        {
+          target_type = "direction",
+          action =
+          {
+            type = "direct",
+            action_delivery =
+            {
+              type = "projectile",
+              projectile = "rocket",
+              starting_speed = 0.1,
+              source_effects =
+              {
+                type = "create-entity",
+                entity_name = "explosion"
+              }
+            }
+          }
+        },
+        magazine_size = 2,
+        subgroup = "ammo",
+        order = "a[basic-clips]-c[uranium-rounds-magazine]",
+        inventory_move_sound = item_sounds.ammo_small_inventory_move,
+        pick_sound = item_sounds.ammo_small_inventory_pickup,
+        drop_sound = item_sounds.ammo_small_inventory_move,
+        stack_size = 100,
+        weight = 40*kg
+    },{
+    type = "gun",
+    name = "halo-reach-rocket-launcher",
+    icon = "__Halo-Mod__/graphics/icons/halo-reach-rocket-launcher.png",
+    icon_size = 64,
+    subgroup = "gun",
+    order = "a[basic-clips]-b[submachine-gun]",
+    inventory_move_sound = item_sounds.weapon_large_inventory_move,
+    pick_sound = item_sounds.weapon_large_inventory_pickup,
+    drop_sound = item_sounds.weapon_large_inventory_move,
+    attack_parameters =
+    {
+      type = "projectile",
+      ammo_category = "halo-reach-rocket-launcher-ammo",
+
+      cooldown = 60,
+      movement_slow_down_factor = 0.7,
+      projectile_creation_distance = 1.125,
+      range = 18,
+      sound = {"__Halo-Mod__/sounds/Halo Reach/rocket_h3_1.wav",
+      "__Halo-Mod__/sounds/Halo Reach/rocket_h3_2.wav",
+      "__Halo-Mod__/sounds/Halo Reach/rocket_h3_3.wav",
+      "__Halo-Mod__/sounds/Halo Reach/rocket_h3_4.wav",}
+    },
+    stack_size = 5
+  },{
+    type = "recipe",
+    name = "halo-reach-rocket-launcher",
+    enabled = true,
+    energy_required = 8, -- time to craft in seconds (at crafting speed 1)
+    ingredients = {
+      {type = "item", name = "rocket-launcher", amount = 1},
+      {type = "item", name = "iron-plate", amount = 1}
+    },
+    results = {{type = "item", name = "halo-reach-rocket-launcher", amount = 1}}
+  },{
+    type = "recipe",
+    name = "halo-reach-rocket-launcher-ammo",
+    enabled = true,
+    energy_required = 1, -- time to craft in seconds (at crafting speed 1)
+    ingredients = {
+      {type = "item", name = "rocket", amount = 2},
+      {type = "item", name = "iron-plate", amount = 1}
+    },
+    results = {{type = "item", name = "halo-reach-rocket-launcher-ammo", amount = 1}}
+  }
+}
